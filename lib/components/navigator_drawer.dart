b5b755c39dd2ba94eec16d20f4b53c4c0a973d20
fcardenasm <fcardenasm@unsa.edu.pe>
2023-11-20 11:59:21 -0500

    Agregada: opcion de cerrar sesión

--- lib/components/navigator_drawer.dart
+++ lib/components/navigator_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mi_medio_pasaje/screens/login_page.dart';
 
 class NavigatorDrawer extends StatelessWidget {
   const NavigatorDrawer({super.key});
@@ -16,13 +17,27 @@ class NavigatorDrawer extends StatelessWidget {
             ),
           ),
           ListTile(
-            title: const Text('Opción 1'),
+            title: const Text('Perfil'),
             onTap: () {},
           ),
           ListTile(
-            title: const Text('Opción 2'),
+            title: const Text('Mis Denuncias'),
             onTap: () {},
           ),
+          ListTile(
+            title: const Text('Sobre nosotros'),
+            onTap: () {},
+          ),
+          InkWell(
+            child: const ListTile(
+              title: Text('Cerrar sesión'),
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()));
+            },
+          ),
+
           // Agregar más ListTile para más opciones
         ],
       ),
