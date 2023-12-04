00e5300a31a8181701c72daba2b68c0ff18bc8e6
fcardenasm <fcardenasm@unsa.edu.pe>
2023-12-04 04:58:28 -0500

    Modificado : admite videos e imagenes

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -5,9 +5,14 @@ Future<String> uploadCloudinary(String rutaArchivo) async {
   String cloudName = dotenv.env['CLOUD_NAME']!;
   String presetName = dotenv.env['PRESET_NAME']!;
   final cloudinary = CloudinaryPublic(cloudName, presetName, cache: false);
+
+  String extension = rutaArchivo.split('.').last;
+  CloudinaryResourceType resourceType = extension == 'mp4' || extension == 'mov'
+      ? CloudinaryResourceType.Video
+      : CloudinaryResourceType.Image;
+
   CloudinaryResponse respuesta = await cloudinary.uploadFile(
-    CloudinaryFile.fromFile(rutaArchivo,
-        resourceType: CloudinaryResourceType.Video),
+    CloudinaryFile.fromFile(rutaArchivo, resourceType: resourceType),
   );
   return respuesta.secureUrl;
 }
