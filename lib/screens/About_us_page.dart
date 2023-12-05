6f74552cfd728afb705cb2e0f087d7c595e49b04
fcardenasm <fcardenasm@unsa.edu.pe>
2023-12-04 21:33:05 -0500

    Modificado: mejoro informacion sobre el equipo

--- lib/screens/About_us_page.dart
+++ lib/screens/About_us_page.dart
@@ -2,37 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:mi_medio_pasaje/components/navigator_drawer.dart';
 
 class AboutUsPage extends StatelessWidget {
+  final List<Map<String, String>> teamMembers = [
+    {'image': 'lib/assets/Franco.jpg', 'name': 'Franco Cárdenas'},
+    {'image': 'lib/assets/Henry.png', 'name': 'Henry Galvéz'}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sobre Nosotros'),
+        title: Text('Sobre Nosotros',
+            style: Theme.of(context).textTheme.titleLarge),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            const SizedBox(height: 40),
             Text(
               'Mi Medio Pasaje',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Text(
               'Mi Medio Pasaje es una aplicación que facilita el acceso a los medios de transporte. '
               'Nuestro objetivo es proporcionar una plataforma fácil de usar que ayude a los usuarios a reclamar su medio pasaje.',
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 32),
             Text(
               'Nuestro Equipo',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Text(
               'Nuestro equipo está compuesto por profesionales dedicados a hacer respetar tus derechos.',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 16),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: teamMembers.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage(teamMembers[index]['image']!),
+                      radius: 50.0,
+                    ),
+                    Text(
+                      teamMembers[index]['name']!,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),
       ),
-      drawer: const NavigatorDrawer(),
     );
   }
 }
